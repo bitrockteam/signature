49c6687d7f13169588afd9c5c7317a98d30da4d2
Salvatore Laisa <moebiusmania@gmail.com>
2019-01-25 11:58:23 +0100

    Feature/intro (#12)
    * add quick instructions
    
    * preparing the amber-code feature

--- src/components/app.ts
+++ src/components/app.ts
@@ -17,6 +17,11 @@ const data :State = {
 const app = () :TemplateResult => html`
   <main class="container">
     <h1>Email signature builder</h1>
+    <p>Just compile the form and then copy the data that your client requires:</p>
+    <ul>
+      <li><b>Gmail:</b> you copy the preview on the right and paste in the signature field.</li>
+      <li><b>Apple Mail:</b> similar to Gmail, but you have to uncheck <i>"Always match my default messsage font"</i>.</li>
+    </ul>
     ${form(data)}
   </main>
 `;
@@ -24,14 +29,15 @@ const app = () :TemplateResult => html`
 const _render = () :void => {
   render(app(), root);
   const code :Element = document.querySelector('pre code');
+  // const AmberCode: Element = document.querySelector('amber-code-snippet');
   const preview :HTMLDivElement = document.querySelector('#preview div');
   code.innerHTML = escapeHtml(preview.innerHTML);
+  // AmberCode.innerHTML = escapeHtml(preview.innerHTML);
 }
 
 document.body.addEventListener('update-data', (evt :CustomEvent) => {
   const det :FieldData = evt.detail;
   data[det.key] = det.value;
-  // console.log(evt.detail, data);
   _render();
 });
 
