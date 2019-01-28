5a9176dc00c610a01f2504a2beaaae021d91c68d
Alessandro Menini <42537124+alessandromenini@users.noreply.github.com>
2019-01-28 11:54:28 +0100

    fixed bold texts behaviour on Apple Mail (#13)

--- src/libs/templates.ts
+++ src/libs/templates.ts
@@ -2,12 +2,12 @@ import { html, TemplateResult } from 'lit-html';
 import { companies } from './companies';
 
 const confidential = () :TemplateResult =>
-  html`<div style="font-size: 12px; line-height: 16px;"><p style="font-size: 12px; line-height: 16px; color: #999999;"><b>CONFIDENTIALITY:</b> This message contains confidential information intended only for the use of the addressee. It’s
+  html`<div style="font-size: 12px; line-height: 16px;"><p style="font-size: 12px; line-height: 16px; color: #999999;"><b style="font-weight: bold;">CONFIDENTIALITY:</b> This message contains confidential information intended only for the use of the addressee. It’s
 intended that any unauthorized use, dissemination or disclosure of this message for purpose that does not comply with
 its purpose is prohibited. If you received this message in error, or are not the intended recipient, please delete it
 and any copies from your systems and immediately notify. Thank you.</p>
 
-<p style="font-size: 12px; line-height: 16px; color: #999999;"><b>AVVISO DI RISERVATEZZA:</b>Le informazioni contenute in questo messaggio sono strettamente riservate ed esclusivamente
+<p style="font-size: 12px; line-height: 16px; color: #999999;"><b style="font-weight: bold;">AVVISO DI RISERVATEZZA:</b>Le informazioni contenute in questo messaggio sono strettamente riservate ed esclusivamente
 indirizzate al destinatario indicato (oppure alla persona responsabile di rimetterlo al destinatario). Resta inteso che
 qualsiasi uso, riproduzione o divulgazione di questo messaggio a fini diversi da quelli oggetto della presente email è
 vietato. Nel caso in cui aveste ricevuto questa mail per errore, vogliate avvertire il mittente al più presto e subito
@@ -32,7 +32,7 @@ const treviso = (company :string) :string|TemplateResult => {
   return company !== companies[2].label ? 
     html`
     <div style="font-size: 12px; line-height: 16px;">
-      <a href="https://www.google.com/maps/place/Viale+della+Repubblica,+156,+31100+Treviso+TV" target="_blank" style="color: #1155cc;"><b>Treviso</b> – Viale della Repubblica 156/A</a>
+      <a href="https://www.google.com/maps/place/Viale+della+Repubblica,+156,+31100+Treviso+TV" target="_blank" style="color: #1155cc;"><b style="font-weight: bold;">Treviso</b> – Viale della Repubblica 156/A</a>
     </div>` : '';
 }
 
@@ -40,7 +40,7 @@ const logo = (company :string) :TemplateResult => {
   const filename :string = company ? company.toLowerCase() : 'bitrock';
   return html`
   <div>
-    <img src="https://github.com/bitrockteam/signature/blob/master/logos/${filename}.png?raw=true" height="32px">
+    <img src="https://github.com/bitrockteam/signature/blob/master/logos/${filename}.png?raw=true" height="32px" nosend="1">
   </div>`;
 }
 
@@ -48,9 +48,9 @@ export const signature = (data :State) :TemplateResult => html`
 <div class="gmail_signature" data-smartmail="gmail_signature">
 <div style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'; font-size: 16px; line-height: 24px; font-weight: normal; color: #1F1F1F;">
   <div>
-    <b style="color: #5C5C5C">${data.firstname}</b>&nbsp;<b style="color: #3D3D3D">${data.lastname}</b>
+    <b style="color: #5C5C5C; font-weight: bold;">${data.firstname}</b>&nbsp;<b style="color: #3D3D3D; font-weight: bold;">${data.lastname}</b>
   </div>
-  <div style="font-size: 12px; line-height: 16px;"><b style="color: #7A7A7A;">${data.role}</b></div>
+  <div style="font-size: 12px; line-height: 16px;"><b style="color: #7A7A7A; font-weight: bold;">${data.role}</b></div>
   <div style="line-height: 16px;"><br></div>
   <div>
     <div style="font-size: 12px; line-height: 16px;;">
@@ -60,10 +60,10 @@ export const signature = (data :State) :TemplateResult => html`
   </div>
   <div style="line-height: 16px;"><br></div>
   <div style="font-size: 14px; line-height: 20px;">
-    <b style="color: #3D3D3D;">${data.company.toUpperCase()}</b>&nbsp;<b style="color: #3D3D3D;">s.r.l.</b>
+    <b style="color: #3D3D3D; font-weight: bold;">${data.company.toUpperCase()}</b>&nbsp;<b style="color: #3D3D3D; font-weight: bold;">s.r.l.</b>
   </div>
   <div style="font-size: 12px; line-height: 16px;">
-    <a href="https://www.google.com/maps/place/Via+Pietro+Borsieri,+41,+20159+Milano+MI" target="_blank" style="color: #1155cc;"><b>Milano</b> – Via Borsieri ${data.company === 'Radicalbit' ? '41' : '41'}</a>
+    <a href="https://www.google.com/maps/place/Via+Pietro+Borsieri,+41,+20159+Milano+MI" target="_blank" style="color: #1155cc;"><b style="font-weight: bold;">Milano</b> – Via Borsieri ${data.company === 'Radicalbit' ? '41' : '41'}</a>
   </div>
   ${treviso(data.company)}
   ${website(data.company)}
